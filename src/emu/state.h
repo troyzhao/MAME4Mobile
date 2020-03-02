32da7c24a816e86d7b6f74277157d7137a41e0b0
troy <troy1139@163.com>
2020-03-02 18:36:25 +0800

    fix undefined namaspace std::tr1

--- src/emu/state.h
+++ src/emu/state.h
@@ -19,7 +19,11 @@
 #define __STATE_H__
 
 #ifdef __GNUC__
+#ifdef IOS
+#include <type_traits>
+#else
 #include <tr1/type_traits>
+#endif
 #endif
 
 
@@ -58,8 +62,13 @@ typedef enum _state_save_error state_save_error;
 
 #ifdef __GNUC__
 #define IS_VALID_SAVE_TYPE(_var) \
+#ifdef IOS
+	(std::is_arithmetic<typeof(_var)>::value || std::is_enum<typeof(_var)>::value || \
+	 std::is_same<typeof(_var), PAIR>::value || std::is_same<typeof(_var), PAIR64>::value)
+#else
 	(std::tr1::is_arithmetic<typeof(_var)>::value || std::tr1::is_enum<typeof(_var)>::value || \
 	 std::tr1::is_same<typeof(_var), PAIR>::value || std::tr1::is_same<typeof(_var), PAIR64>::value)
+#endif
 #else
 #define IS_VALID_SAVE_TYPE(_var) \
 	(sizeof(_var) == 1 || sizeof(_var) == 2 || sizeof(_var) == 4 || sizeof(_var) == 8)
